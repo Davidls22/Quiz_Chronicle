6f397bb2081681cf6897b427dee742b5d6d0bb43
David Sugden <david-sugden1@hotmail.co.uk>
2023-12-29 17:57:59 +0000

    Initial Commit

---
+++ Quiz Chronicle/Quiz_ChronicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_ChronicleApp.swift
+//  Quiz Chronicle
+//
+//  Created by David Sugden on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_ChronicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
