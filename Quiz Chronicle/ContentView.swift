39eabc8e18075eb28b0a21a6354a973536f19be5
David Sugden <david-sugden1@hotmail.co.uk>
2024-01-05 19:49:32 +0000

    changed colour scheme, added first question, multiple choice buttons and image

--- Quiz Chronicle/ContentView.swift
+++ Quiz Chronicle/ContentView.swift
@@ -8,22 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let mainColor = Color(red: 255/255, green: 120/255, blue: 80/255)
+    let accentColor = Color(red: 40/255, green: 170/255, blue: 110/255)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "checkmark.circle")
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(Color.gray)
-                .imageScale(.large)
-                .scaleEffect(1.5)
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                        .font(.callout)
+                        .multilineTextAlignment(.leading)
+                        .padding()
+                Text("Who is the Author of 'The Lord of the Rings'?")
+                        .font(.title)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding()
+                Spacer()
                 
-            Text("Quiz Chronicle")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
+                Image("JRRTolkein")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
                 
-                
-                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        print("Tapped on Choice 1")
+                    }, label: {
+                        Text("JRR Tolkien")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(accentColor)
+                            .foregroundColor(Color.white) // Adjust text color for visibility
+                            .cornerRadius(8)
+                    })
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        print("Tapped on Choice 2")
+                    }, label: {
+                        Text("JK Rowling")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(accentColor)
+                            .foregroundColor(Color.white) // Adjust text color for visibility
+                            .cornerRadius(8)
+                    })
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        print("Tapped on Choice 3")
+                    }, label: {
+                        Text("George RR Martin")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(accentColor)
+                            .foregroundColor(Color.white) // Adjust text color for visibility
+                            .cornerRadius(8)
+                    })
+                    
+                    Spacer()
+                }
+            }
         }
-        .padding()
     }
 }
 
